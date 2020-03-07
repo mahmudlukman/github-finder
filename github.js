a3e080a1d2d06936b46a2a96b381692320b384e4
Lukman Mahmud <mistamann49@gmail.com>
2020-03-07 21:52:51 +0100

    init commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+    constructor() {
+        this.client_id = '1e362afcdbd480558025';
+        this.client_secret = '4e5f33021fe032ee9f2698d347cb9a8304731f17';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
